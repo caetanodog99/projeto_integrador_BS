1a0bf2b0785ee85e927ad970d27958ce3ff1bc2e
caetanodog99 <caetanouniso@gmail.com>
2026-04-14 22:03:46 -0300

    TD beta (botao de voltar pro menu)

--- TowerDefense/Assets/scripts/regras/playManager.cs
+++ TowerDefense/Assets/scripts/regras/playManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.TerrainTools;
 
 public class playManager : MonoBehaviour
@@ -8,7 +9,7 @@ public class playManager : MonoBehaviour
     [SerializeField] public GameObject painelPause;
     [SerializeField] public GameObject painelLoja;
 
-    public void BotaoPause()
+public void BotaoPause()
     {
         Time.timeScale = 0;
         painelPause.SetActive(true);
@@ -28,6 +29,13 @@ public class playManager : MonoBehaviour
     public void FecharLoja()
     {
         painelLoja.SetActive(false);
+    }
+
+    public void VoltarMenu()
+    {
+        int cena = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.UnloadSceneAsync(cena);
+        SceneManager.LoadSceneAsync(0);
     }
 
     private void Update()
